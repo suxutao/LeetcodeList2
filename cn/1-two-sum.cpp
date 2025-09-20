e1df7a25cbad9a96f6ff8501030fc39d3d8ce867
suxutao <2370746839@qq.com>
2025-09-20 11:17:17 +0800

    2025.9.20

---
+++ cn/1-two-sum.cpp
@@ -0,0 +1,22 @@
+#include "../../../stdc.h"
+using namespace std;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int>v;
+        for (int i = 0; i < 1; ++i) {
+
+        }
+        return {0};
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+int main() {
+    vector<int>v{1};
+    Solution().twoSum(v,1);
+    return 0;
+}
